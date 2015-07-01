ee79e558ee34e1173a90b2acfd5545dc5eb3c8e5
tarena19 <tarena19@HELLO.local>
2015-07-01 12:19:08 +0800

    add log

---
+++ demo4git/demo4git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo4git
+//
+//  Created by tarena19 on 15/7/1.
+//  Copyright (c) 2015年 Tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo4git/demo4git/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demo4git
+//
+//  Created by tarena19 on 15/7/1.
+//  Copyright (c) 2015年 Tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"this is first project");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
